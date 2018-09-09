436b1d8b6d4541ca5e7d407c974175315125d29d
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-09 21:41:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,8 +40,8 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray (array) {
-  newArray = array.slice(0, array.length-1)
-  return newArray
+  array = array.slice(0, array.length-1)
+  return array
 }
 
 
